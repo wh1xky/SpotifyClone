a7c0c4472de36c0a1c6f012194fd3f5297d460ed
wh1sky <yvonnedo2809@gmail.com>
2023-12-17 19:45:57 +0700

    Update Sidebar

---
+++ components/SidebarItem.tsx
@@ -0,0 +1,29 @@
+import {IconType} from "react-icons";
+import React from "react";
+import Link from "next/link";
+import {twMerge} from "tailwind-merge";
+
+interface SidebarItemProps {
+    icon: IconType;
+    label: string;
+    active?: boolean;
+    href: string;
+}
+
+const SidebarItem: React.FC<SidebarItemProps> = ({
+    icon: Icon,
+    label,
+    active,
+    href
+}) => {
+    return (
+        <div>
+            <Link href={href} className={twMerge(`flex flex-row h-auto items-center w-full gap-x-4 text-md font-medium cursor-pointer hover:text-white transition text-neutral-400 py-1`, active && "text-white" )}>
+                <Icon size={26}/>
+                    <p className="truncate w-full">{label}</p>
+            </Link>
+        </div>
+    )
+}
+
+export default SidebarItem
